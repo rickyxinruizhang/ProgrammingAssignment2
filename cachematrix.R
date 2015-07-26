b6f2925892597652c2476ee9414521e6c720c452
ZHANG XINRUI <ricky.xinruizhang@gmail.com>
2015-07-26 17:18:24 +0900

    The comment format modification

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,6 @@
-## These two functions are written to make a special version of matrix and cache
+## These two functions are written to make a special version of matrix and cachethe inverse of a Matrix to avoid repeatedly computing the inverse.
 
-##the inverse of a Matrix to avoid repeatedly computing the inverse.
-
-## The makeCacheMatrix function is designed to create a special "matrix" using 
-## the initial matrix value of x, which is really a list containing a function to 
+## The makeCacheMatrix function is designed to create a special "matrix" using the initial matrix value of x, which is really a list containing a function to 
 ## 1. set the value of the matrix
 ## 2. get the value of the matrix
 ## 3. set the value of the inverse
@@ -16,7 +13,7 @@ makeCacheMatrix <- function(x = matrix()) {
 	set<-function(y){
 		x<<-y
 		m<<-NULL
-                ##if the value of set is updated, set m to be NULL again because                ##the inverse would be calculated again.
+                ##if the value of set is updated, set m to be NULL again because the inverse would be calculated again.
 	}
 	get<-function() x
 	setinv<-function(inv) m<<-inv
@@ -28,13 +25,13 @@ makeCacheMatrix <- function(x = matrix()) {
 ##    The cacheSolve function returns a matrix that is the inverse of "x"
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x', be careful that here x is        ## the special matrix defined by function makeCacheMatrix
+        ## Return a matrix that is the inverse of 'x', be careful that here x is the special matrix defined by function makeCacheMatrix
 	m<-x$getinv()
         ## m is used to store the inverse
 	if(!is.null(m)){
 		message("getting cached data")
 		return(m)
-                ## If the cache value of inverse of "x" is not NULL, which mean                  s the same "matrix"'s inverse has been calculated before, henc                  e return it  
+                ## If the cache value of inverse of "x" is not NULL, which means the same "matrix"'s inverse has been calculated before, hence return it  
 	}
 	data<-x$get()
 	m<-solve(x$get()) ## recalculate the inverse
